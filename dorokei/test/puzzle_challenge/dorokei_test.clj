3e059bed6dd30345b22afcce60ca13599dc1f0e7
OHTA Shogo <athos0220@gmail.com>
2017-09-03 13:13:25 +0900

    Add new project for today's problem: dorokei

---
+++ dorokei/test/puzzle_challenge/dorokei_test.clj
@@ -0,0 +1,7 @@
+(ns puzzle-challenge.dorokei-test
+  (:require [clojure.test :refer :all]
+            [puzzle-challenge.dorokei :as dorokei]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
