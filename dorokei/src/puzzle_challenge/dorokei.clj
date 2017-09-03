3f5b717cdee0eff366374ad2ebb8102d6e97e5ec
OHTA Shogo <athos0220@gmail.com>
2017-09-03 15:26:39 +0900

    Implement algorithm to solve dorokei

--- dorokei/src/puzzle_challenge/dorokei.clj
+++ dorokei/src/puzzle_challenge/dorokei.clj
@@ -1,6 +1,52 @@
-(ns puzzle-challenge.dorokei)
+(ns puzzle-challenge.dorokei
+  (:import [clojure.lang PersistentQueue]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def ^:dynamic *nodes*
+  {0 [1 3 4]
+   1 [2 5]
+   2 [1 3]
+   3 [0 2 4]
+   4 [0 3 5]
+   5 [1 4]})
+
+(defn- opponent-of [turn]
+  (case turn
+    :cop :robber
+    :cop))
+
+(defn step [{:keys [record path] :as state}]
+  (let [{:keys [turn cop]} record]
+    (keep (fn [node]
+            (when (or (not= turn :robber) (not= cop node))
+              (assoc state
+                     :record (assoc record
+                                    :turn (opponent-of turn)
+                                    turn node)
+                     :path (conj path record))))
+          (get *nodes* (get-in state [:record turn])))))
+
+(defn bad? [{:keys [cop robber turn]}]
+  (and (= turn :cop)
+       (some #{robber} (get *nodes* cop))))
+
+(defn win? [{:keys [cop robber turn]}]
+  (and (= turn :robber)
+       (= cop 0)
+       (#{2 5} robber)))
+
+(defn solve
+  ([init]
+   (solve (conj PersistentQueue/EMPTY [{:record init :path []}]) #{}))
+  ([queue visited]
+   (let [states (peek queue), queue (pop queue)]
+     (when states
+       (if-let [state (some (fn [{:keys [record] :as state}]
+                              (and (win? record) state))
+                            states)]
+         (conj (:path state) (:record state))
+         (let [states' (->> (mapcat step states)
+                            (remove (fn [{:keys [record]}]
+                                      (or (visited record)
+                                          (bad? record)))))]
+           (recur (conj queue states')
+                  (into visited (map :record) states'))))))))
